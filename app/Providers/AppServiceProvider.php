3ad8a99e0dff47aa1918d790f0063b57ed577cc1
Ali <aliahmadi1999001378@gmail.com>
2026-09-12 11:18:29 +0330

    add Schema::defaultStringLength(191);

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+//        حتماً. این خط در لاراول خیلی مهمه، مخصوصاً اگر با MySQL و نسخه‌های قدیمی‌تر یا تنظیمات خاص دیتابیس کار کنی.
+//    وقتی من یک ستون string می‌سازم و طولش رو مشخص نکردم، طول پیش‌فرضش رو 191 کاراکتر در نظر بگیر.
+//    $table->string('name');
+//        در حالت معمول Laravel این رو به چیزی شبیه این تبدیل می‌کنه:
+//        VARCHAR(255)
+//            Laravel طول پیش‌فرض string رو به 191 کاهش می‌ده:
+//        دلیلش بیشتر به MySQL و Index مربوط می‌شه.
+//
+//    در بعضی نسخه‌ها/تنظیمات قدیمی MySQL، وقتی از utf8mb4 استفاده می‌کردیم، یک ستون:
+//        VARCHAR(255)
+//            Specified key was too long; max key length is 767 bytes
+
+        Schema::defaultStringLength(191);
+
+
+
+
+
     }
 }
